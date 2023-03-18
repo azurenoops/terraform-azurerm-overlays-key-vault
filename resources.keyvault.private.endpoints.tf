8985c2593381c928453c02e38ef0a53f1bd7b254
John Spinella <jrspinella@users.noreply.github.com>
2023-03-17 20:59:49 -0400

    Update PLink

--- resources.keyvault.private.endpoints.tf
+++ resources.keyvault.private.endpoints.tf
@@ -2,12 +2,21 @@
 # Licensed under the MIT License.
 
 #---------------------------------------------------------
-# Private Link for Storage Account - Default is "false" 
+# Private Link for Sql - Default is "false" 
 #---------------------------------------------------------
 data "azurerm_virtual_network" "vnet" {
-  count               = var.enable_private_endpoint ? 1 : 0
+  count               = var.enable_private_endpoint && var.existing_vnet_id == null ? 1 : 0
   name                = var.virtual_network_name
   resource_group_name = local.resource_group_name
+}
+
+resource "azurerm_subnet" "snet_ep" {
+  count                                     = var.enable_private_endpoint && var.existing_subnet_id == null ? 1 : 0
+  name                                      = "snet-endpoint-${local.location}"
+  resource_group_name                       = var.existing_vnet_id == null ? data.azurerm_virtual_network.vnet.0.resource_group_name : element(split("/", var.existing_vnet_id), 4)
+  virtual_network_name                      = var.existing_vnet_id == null ? data.azurerm_virtual_network.vnet.0.name : element(split("/", var.existing_vnet_id), 8)
+  address_prefixes                          = var.private_subnet_address_prefix
+  private_endpoint_network_policies_enabled = true
 }
 
 resource "azurerm_private_endpoint" "pep" {
@@ -21,11 +30,14 @@ resource "azurerm_private_endpoint" "pep" {
   private_service_connection {
     name                           = "keyvault-privatelink"
     is_manual_connection           = false
-    private_connection_resource_id = element([for i in azurerm_key_vault.keyvault : i.id], 0)
+    private_connection_resource_id = azurerm_key_vault.keyvault.id
     subresource_names              = ["vault"]
   }
 }
 
+#------------------------------------------------------------------
+# DNS zone & records for SQL Private endpoints - Default is "false" 
+#------------------------------------------------------------------
 data "azurerm_private_endpoint_connection" "pip" {
   count               = var.enable_private_endpoint ? 1 : 0
   name                = azurerm_private_endpoint.pep.0.name
@@ -37,21 +49,22 @@ resource "azurerm_private_dns_zone" "dns_zone" {
   count               = var.existing_private_dns_zone == null && var.enable_private_endpoint ? 1 : 0
   name                = var.environment == "public" ? "privatelink.vaultcore.azure.net" : "privatelink.vaultcore.usgovcloudapi.net"
   resource_group_name = local.resource_group_name
-  tags                = merge({ "Name" = format("%s", "Key-Vault-Private-DNS-Zone") }, var.add_tags, )
+  tags                = merge({ "Name" = format("%s", "Azure-Key-Vault-Private-DNS-Zone") }, var.add_tags, )
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link" {
   count                 = var.existing_private_dns_zone == null && var.enable_private_endpoint ? 1 : 0
   name                  = "vnet-private-zone-link"
   resource_group_name   = local.resource_group_name
-  private_dns_zone_name = azurerm_private_dns_zone.dns_zone.0.name
-  virtual_network_id    = data.azurerm_virtual_network.vnet.0.id
+  private_dns_zone_name = var.existing_private_dns_zone == null ? azurerm_private_dns_zone.dns_zone.0.name : var.existing_private_dns_zone
+  virtual_network_id    = var.existing_vnet_id == null ? data.azurerm_virtual_network.vnet.0.id : var.existing_vnet_id
+  registration_enabled  = true
   tags                  = merge({ "Name" = format("%s", "vnet-private-zone-link") }, var.add_tags, )
 }
 
 resource "azurerm_private_dns_a_record" "a_rec" {
   count               = var.enable_private_endpoint ? 1 : 0
-  name                = element([for n in azurerm_key_vault.keyvault : n.name], 0)
+  name                = azurerm_key_vault.keyvault.name
   zone_name           = var.existing_private_dns_zone == null ? azurerm_private_dns_zone.dns_zone.0.name : var.existing_private_dns_zone
   resource_group_name = local.resource_group_name
   ttl                 = 300
