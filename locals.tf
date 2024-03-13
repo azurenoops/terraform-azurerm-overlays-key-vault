8c6924e20e3aea7f419c21c7c4e7c9ca582d4302
John Spinella <jrspinella@users.noreply.github.com>
2024-03-13 14:14:53 -0400

    updare dns name

--- locals.tf
+++ locals.tf
@@ -14,5 +14,5 @@ locals {
 ##-----------------------------------------------------------------------------
 locals {
   valid_rg_name         = var.existing_private_dns_zone == null ? local.resource_group_name : var.existing_private_dns_zone_resource_group_name
-  private_dns_zone_name = var.enable_private_endpoint ? var.existing_private_dns_zone == null ? azurerm_private_dns_zone.dnszone[0].name : var.existing_private_dns_zone : null
+  private_dns_zone_name = var.enable_private_endpoint ? var.existing_private_dns_zone == null ? azurerm_private_dns_zone.dns_zone[0].name : var.existing_private_dns_zone : null
 }
