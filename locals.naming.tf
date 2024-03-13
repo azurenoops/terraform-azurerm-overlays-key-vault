975bdc817f1c7981eb19629147f19b09684627f9
John Spinella <jrspinella@users.noreply.github.com>
2024-03-13 12:57:14 -0400

    Add names for DNS

--- locals.naming.tf
+++ locals.naming.tf
@@ -8,6 +8,7 @@ locals {
 
   resource_group_name = element(coalescelist(data.azurerm_resource_group.rgrp.*.name, module.mod_key_vault_rg.*.resource_group_name, [""]), 0)
   location            = element(coalescelist(data.azurerm_resource_group.rgrp.*.location, module.mod_key_vault_rg.*.resource_group_location, [""]), 0)
-  kv_name                = coalesce(var.custom_name, data.azurenoopsutils_resource_name.keyvault.result)
-  hsm_name            = coalesce(var.custom_name, data.azurenoopsutils_resource_name.keyvault_hsm.result)
+  kv_name                = coalesce(var.custom_kv_name, data.azurenoopsutils_resource_name.keyvault.result)
+  hsm_name            = coalesce(var.custom_hsm_name, data.azurenoopsutils_resource_name.keyvault_hsm.result)
+  private_dns_a_record_name = coalesce(var.custom_private_dns_a_record_name, data.azurenoopsutils_resource_name.keyvault_dns_a_record.result)
 }
